8ec0610f4170a3139db858b0362b97290cfd96a4
Kanchan012 <lawojukanchan@gmail.com>
2026-01-05 10:41:24 +0000

    grader added

---
+++ Problemset1/q3.rb
@@ -0,0 +1,14 @@
+def grader(score)
+  return "A+" if score >= 90
+  return "A"  if score >= 80
+  return "B+" if score >= 70
+  return "B"  if score >= 60
+  return "C+" if score >= 50
+  return "C"  if score >= 40
+  "F"
+end
+puts grader(95) 
+puts grader(0) 
+puts grader(45) 
+puts grader(60) 
+ 
